5ef5c37917875dd65a3563899d8aedade12e3048
Yannick Bensacq <yannick.bensacq@tagmaster.com>
2021-03-22 23:44:05 +0100

    e

--- src/handlers/nft.ts
+++ src/handlers/nft.ts
@@ -5,6 +5,7 @@ import {NftEntity} from "../types/models/NftEntity";
 
 export const listHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
   const { extrinsic: _extrinsic } = extrinsic
+  console.log(call.args);
 
 }
 
@@ -13,9 +14,15 @@ export const createHandler: ExtrinsicHandler = async (call, extrinsic): Promise<
 
   const signer = _extrinsic.signer.toString()
   const [to, amount] = call.args
+  console.log(call.args);
+
   const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
   const nftRecord = new NftEntity(commonExtrinsicData.hash)
 
   // apply common extrinsic data to record
   insertDataToEntity(nftRecord, commonExtrinsicData)
+  nftRecord.owner = signer.toString();
+  nftRecord.currency = 'CAPS';
+  nftRecord.listed = 0;
+
 }
---
+++ src/types/models/NftEntity.ts
@@ -0,0 +1,55 @@
+// Copyright 2020-2021 OnFinality Limited authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// Auto-generated , DO NOT EDIT
+import {Entity} from "@subql/types";
+import assert from 'assert';
+
+export class NftEntity implements Entity {
+
+    constructor(id: string) {
+        this.id = id;
+    }
+
+
+    public id: string;
+
+    public uri?: string;
+
+    public timestampList?: Date;
+
+    public currency: string;
+
+    public price?: string;
+
+    public listed: number;
+
+    public owner: string;
+
+
+    async save(): Promise<void>{
+        let id = this.id;
+        assert(id !== null, "Cannot save NftEntity entity without an ID");
+        await store.set('NftEntity', id.toString(), this);
+    }
+    static async remove(id:string): Promise<void>{
+        assert(id !== null, "Cannot remove NftEntity entity without an ID");
+        await store.remove('NftEntity', id.toString());
+    }
+
+    static async get(id:string): Promise<NftEntity>{
+        assert(id !== null, "Cannot get NftEntity entity without an ID");
+        const record = await store.get('NftEntity', id.toString());
+        if (record){
+            return NftEntity.create(record);
+        }else{
+            return;
+        }
+    }
+
+    static create(record){
+        let entity = new NftEntity(record.id);
+        Object.assign(entity,record);
+        return entity;
+    }
+}
