1899fe5cfffaf6ae59500836f73d359cd73041d7
Mohsin Riaz <bahrianboy@gmail.com>
2021-07-26 18:08:05 +0500

    fix batch listing and batch burn

--- src/handlers/nft.ts
+++ src/handlers/nft.ts
@@ -1,86 +1,82 @@
-import {insertDataToEntity, getCommonExtrinsicData, updateAccount} from '../helpers'
+import { insertDataToEntity, getCommonExtrinsicData, updateAccount } from '../helpers'
 import { ExtrinsicHandler } from './types'
 import { Balance } from "@polkadot/types/interfaces";
 import { bnToBn } from '@polkadot/util/bn';
 import { NftEntity } from "../types/models/NftEntity";
 import { TransferEntity } from "../types/models/TransferEntity";
 
-  export const createHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
-    const { extrinsic: _extrinsic, events } = extrinsic
-    const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
-    const record = new NftEntity(commonExtrinsicData.hash)
-    logger.info('Create Nft '+commonExtrinsicData.block);
+export const createHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
+  const { extrinsic: _extrinsic, events } = extrinsic
+  const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
+  const record = new NftEntity(commonExtrinsicData.hash)
+  logger.info('Create Nft ' + commonExtrinsicData.block);
 
-    for (const {event: {data, method, section}} of events) {
-      if (`${section}.${method}` === 'nfts.Created') {
-        // apply common extrinsic data to record
-        insertDataToEntity(record, commonExtrinsicData)
-        const signer = _extrinsic.signer.toString()
-        const nftId = data[0].toString()
-        const nftData = await api.query.nfts.data(nftId);
-        record.currency = 'CAPS';
-        record.listed = 0;
-        record.owner = signer;
-        record.serieId = data[2].toString();
-        record.creator = signer;
-        record.id = nftId;
-        // @ts-ignore
-        const offchain_uri = Buffer.from(nftData.details.offchain_uri, 'hex');
-        record.uri = offchain_uri.toString();
-        await record.save()
-      }
-    }
-
-  }
-
-  export const listHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
-    const {extrinsic: _extrinsic, events} = extrinsic
-    const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
-    logger.info('List Nft '+commonExtrinsicData.block);
-
-    if (events.length > 0 && events[0].event !== undefined) {
-      const nftId = events[0].event.data[0].toString()
-
-      let price = '';
-      let priceTiime = '';
-      const priceObject = JSON.parse(events[0].event.data[1].toString());
-      if(priceObject.caps) {
-        price = bnToBn(priceObject.caps).toString();
-      } else if (priceObject.tiime) {
-        priceTiime = bnToBn(priceObject.tiime).toString();
-      }else if (priceObject.combined !== undefined) {
-        price = bnToBn(priceObject.combined.caps).toString();
-        priceTiime = bnToBn(priceObject.combined.tiime).toString();
-
-      }
-      // retieve the nft
-      const record = await NftEntity.get(nftId);
-      if (record !== undefined) {
-        record.listed = 1;
-        record.timestampList = new Date();
-
-        try {
-          record.price = price
-          record.priceTiime = priceTiime
-          await record.save()
-        } catch (e) {
-          // @ts-ignore
-          logger.error(events[0].event.data[1].toString());
-        }
-      }
+  for (const { event: { data, method, section } } of events) {
+    if (`${section}.${method}` === 'nfts.Created') {
+      // apply common extrinsic data to record
+      insertDataToEntity(record, commonExtrinsicData)
+      const signer = _extrinsic.signer.toString()
+      const nftId = data[0].toString()
+      const nftData = await api.query.nfts.data(nftId);
+      record.currency = 'CAPS';
+      record.listed = 0;
+      record.owner = signer;
+      record.serieId = data[2].toString();
+      record.creator = signer;
+      record.id = nftId;
+      // @ts-ignore
+      const offchain_uri = Buffer.from(nftData.details.offchain_uri, 'hex');
+      record.uri = offchain_uri.toString();
+      await record.save()
     }
   }
+
+}
+
+export const listHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
+  const { extrinsic: _extrinsic, events } = extrinsic
+  const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
+  const [nftId, priceObject] = call.args
+  logger.info('nftId:' + nftId + ':new List Nft ' + commonExtrinsicData.block);
+  let price = '';
+  let priceTiime = '';
+  if (priceObject.caps) {
+    price = bnToBn(priceObject.caps).toString();
+  } else if (priceObject.tiime) {
+    priceTiime = bnToBn(priceObject.tiime).toString();
+  } else if (priceObject.combined !== undefined) {
+    price = bnToBn(priceObject.combined.caps).toString();
+    priceTiime = bnToBn(priceObject.combined.tiime).toString();
+
+  }
+  // retieve the nft
+  const record = await NftEntity.get(nftId);
+  if (record !== undefined) {
+    record.listed = 1;
+    record.timestampList = new Date();
+
+    try {
+      record.price = price
+      record.priceTiime = priceTiime
+      await record.save()
+    } catch (e) {
+      // @ts-ignore
+      logger.error('list nft error:' + nftId);
+    }
+  }
+
+}
 
 export const buyHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
-  const {extrinsic: _extrinsic, events} = extrinsic;
+  const { extrinsic: _extrinsic, events } = extrinsic;
   const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
-  logger.info('Buy Nft '+commonExtrinsicData.block);
-  for (const {event: {data, method, section}} of events) {
+  logger.info('Buy Nft ' + commonExtrinsicData.block);
+  for (const { event: { data, method, section } } of events) {
     if (`${section}.${method}` === 'balances.Transfer') {
       // transfer
       const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
       const transferRecord = new TransferEntity(commonExtrinsicData.hash)
-      const [from,to, amount] = data;
+      const [from, to, amount] = data;
       // apply common extrinsic data to record
       insertDataToEntity(transferRecord, commonExtrinsicData)
       transferRecord.from = from.toString()
@@ -93,7 +89,7 @@ export const buyHandler: ExtrinsicHandler = async (call, extrinsic): Promise<voi
 
       await transferRecord.save()
 
-    }else if (`${section}.${method}` === 'marketplace.NftSold'){
+    } else if (`${section}.${method}` === 'marketplace.NftSold') {
       // sold event
       const nftId = data[0].toString()
       const signer = _extrinsic.signer.toString()
@@ -108,27 +104,21 @@ export const buyHandler: ExtrinsicHandler = async (call, extrinsic): Promise<voi
     }
 
   }
-
 }
 
 export const NFTtransferHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
   const { extrinsic: _extrinsic, events } = extrinsic
   const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
-  logger.info('Transfer Nft '+commonExtrinsicData.block);
-  if(events.length > 0 && events[0].event !== undefined ){
-    console.log('events[0].event.data',events[0].event.data)
-    const nftId = events[0].event.data[0].toString()
-    const oldOwner = events[0].event.data[1].toString()
-    const newOwner = events[0].event.data[2].toString()
+  const [nftId, oldOwner, newOwner] = call.args
+  logger.info('Transfer Nft ' + commonExtrinsicData.block);
 
-    // retrieve the nft
-    const record = await NftEntity.get(nftId);
-    if( record !== undefined ){
-      record.listed = 0;
-      record.owner = newOwner
+  // retrieve the nft
+  const record = await NftEntity.get(nftId);
+  if (record !== undefined) {
+    record.listed = 0;
+    record.owner = newOwner
 
-      await record.save()
-    }
+    await record.save()
   }
 
 }
@@ -136,17 +126,13 @@ export const NFTtransferHandler: ExtrinsicHandler = async (call, extrinsic): Pro
 export const burnHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
   const { extrinsic: _extrinsic, events } = extrinsic
   const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
-  logger.info('burn Nft '+commonExtrinsicData.block);
-  if(events.length > 0 && events[0].event !== undefined ){
-    const nftId = events[0].event.data[0].toString()
-
-    // retrieve the nft
-    const record = await NftEntity.get(nftId);
-    if( record !== undefined ){
-      record.listed = 0;
-      record.timestampBurn = new Date();
-      await record.save()
-    }
+  const [nftId] = call.args
+  logger.info('burn Nft id' + nftId + ' block' + commonExtrinsicData.block);
+  // retrieve the nft
+  const record = await NftEntity.get(nftId);
+  if (record !== undefined) {
+    record.listed = 0;
+    record.timestampBurn = new Date();
+    await record.save()
   }
-
 }
