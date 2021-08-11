ef2ef5dd38f3e516a91a238f4b0e40ef701b40e4
Loïc Tosser <loic@kalvad.com>
2021-08-11 10:25:23 +0400

    Better deployment again

--- run.sh
+++ run.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
+
 [ -z "$APP_HOME" ] && export APP_HOME=$(pwd)
-[ -z "$DB_USER" ] && export DB_USER="postgres"
-[ -z "$DB_PASS" ] && export DB_PASS="postgres"
-[ -z "$DB_DATABASE" ] && export DB_DATABASE="ternoa"
-[ -z "$DB_HOST" ] && export DB_HOST="postgres"
-[ -z "$DB_PORT" ] && export DB_PORT="5432"
+
+[ -z "$POSTGRESQL_ADDON_USER" ] && export POSTGRESQL_ADDON_USER="postgres"
+[ -z "$DB_USER" ] && export DB_USER="$POSTGRESQL_ADDON_USER"
+
+[ -z "$POSTGRESQL_ADDON_PASSWORD" ] && export POSTGRESQL_ADDON_PASSWORD="postgres"
+[ -z "$DB_PASS" ] && export DB_PASS="$POSTGRESQL_ADDON_PASSWORD"
+
+[ -z "$POSTGRESQL_ADDON_DB" ] && export POSTGRESQL_ADDON_DB="postgres"
+[ -z "$DB_DATABASE" ] && export DB_DATABASE="$POSTGRESQL_ADDON_DB"
+
+[ -z "$POSTGRESQL_ADDON_HOST" ] && export POSTGRESQL_ADDON_HOST="localhost"
+[ -z "$DB_HOST" ] && export DB_HOST="$POSTGRESQL_ADDON_HOST"
+
+[ -z "$POSTGRESQL_ADDON_PORT" ] && export POSTGRESQL_ADDON_PORT="5432"
+[ -z "$DB_PORT" ] && export DB_PORT="$POSTGRESQL_ADDON_PORT"
+
+env | grep DB_
+
 
 subql-query --name subql-ternoa --playground
