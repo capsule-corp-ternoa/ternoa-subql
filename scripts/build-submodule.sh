6bdd130be084788721aaaea7b8902c81cc5c615a
SOUFIANE BOUCHAARA <soufiane.devc@gmail.com>
2021-09-10 15:50:57 +0400

    Update build-submodule.sh

--- scripts/build-submodule.sh
+++ scripts/build-submodule.sh
@@ -1,9 +1,30 @@
 git submodule update --init
 
-cd subql/packages/query
-
+cd ../../..
+cd subql/packages/types
 npm install
 npm run build
+
+cd subql/packages/common
+npm install
+npm run build
+
+cd ../../..
+cd subql/packages/validator
+npm install
+npm run build
+
+cd ../../..
+cd subql/packages/query
+npm install
+npm run build
+
+cd ../../..
+cd subql/packages/node
+npm install
+npm run build
+
+
 # cp bin/run bin/ternoa-query
 
 cd ../../..
