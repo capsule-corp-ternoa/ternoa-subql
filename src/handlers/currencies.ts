bb0d7957de5225fe437f1867d650b02cc91a3247
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2022-02-07 18:16:43 +0100

    improve error management

--- src/handlers/currencies.ts
+++ src/handlers/currencies.ts
@@ -3,16 +3,20 @@ import { ExtrinsicHandler } from './types'
 import { genericTransferHandler } from '.';
 
 export const transferHandler: ExtrinsicHandler = async (call, extrinsic): Promise<void> => {
-  const { extrinsic: _extrinsic } = extrinsic
-  const signer = _extrinsic.signer.toString()
-  const [to, amount] = call.args
-  const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
-  const methodEvents = extrinsic.events.filter(x => x.event.section === "balances" && x.event.method === "Transfer")
-  const event = methodEvents[call.batchMethodIndex || 0]
-  const extrinsicIndex = event.phase.isApplyExtrinsic ? event.phase.asApplyExtrinsic.toNumber() : 0
-  await genericTransferHandler(signer.toString(), to.toString(), amount, commonExtrinsicData, call.batchMethodIndex || 0, extrinsicIndex)
-  logger.info('transfer');
-  // update account
-  await updateAccount(to.toString());
-  await updateAccount(signer.toString());
+  try{
+    const { extrinsic: _extrinsic } = extrinsic
+    const signer = _extrinsic.signer.toString()
+    const [to, amount] = call.args
+    const commonExtrinsicData = getCommonExtrinsicData(call, extrinsic)
+    const methodEvents = extrinsic.events.filter(x => x.event.section === "balances" && x.event.method === "Transfer")
+    const event = methodEvents[call.batchMethodIndex || 0]
+    const extrinsicIndex = event.phase.isApplyExtrinsic ? event.phase.asApplyExtrinsic.toNumber() : 0
+    await genericTransferHandler(signer.toString(), to.toString(), amount, commonExtrinsicData, call.batchMethodIndex || 0, extrinsicIndex)
+    logger.info('transfer');
+    // update account
+    await updateAccount(to.toString());
+    await updateAccount(signer.toString());
+  }catch(err){
+    logger.error("Error in transferHandler : " + JSON.stringify(err))
+  }
 }
--- src/helpers/account.ts
+++ src/helpers/account.ts
@@ -32,7 +32,7 @@ export const updateAccount = async (user: string) => {
       }
     });
   } catch (e) {
-    logger.error(e.toString());
+    logger.error(JSON.stringify(e));
   }
 
 }
