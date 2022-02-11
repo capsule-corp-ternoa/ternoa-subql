9f6e94ff4737c1e0845c28c7e5e21cbbef5b8986
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2022-02-11 16:01:37 +0100

    types

--- src/helpers/account.ts
+++ src/helpers/account.ts
@@ -3,36 +3,34 @@ import { roundPrice } from "../utils";
 
 export const updateAccount = async (user: string) => {
   try {
-    const date = new Date()
-    // retrieve the user
-    let record = await AccountEntity.get(user);
-    if( record === undefined ){
-      record = new AccountEntity(user)
-      record.createdAt = date
-    }
-    await api.query.system.account(user, async ({ data: balance })  =>  {
-      try{
-        const balanceFrozenFee = balance.feeFrozen.toBigInt()
-        const balanceFrozenMisc = balance.miscFrozen.toBigInt()
-        const balanceReserved = balance.reserved.toBigInt()
-        const balanceFree = balance.free.toBigInt()
-        const frozen = balanceFrozenFee > balanceFrozenMisc ? balanceFrozenMisc : balanceFrozenMisc
-        const total = balanceFree + balanceReserved
-        const transferable = balanceFree - frozen
-        record.capsAmount = transferable.toString();
-        record.capsAmountFrozen = frozen.toString();
-        record.capsAmountTotal = total.toString();
-        record.capsAmountRounded = roundPrice(record.capsAmount)
-        record.capsAmountFrozenRounded = roundPrice(record.capsAmountFrozen)
-        record.capsAmountTotalRounded = roundPrice(record.capsAmountTotal)
-        record.updatedAt = date
-        await record.save();
-      }catch(err){
-        logger.error(JSON.stringify(err))
+    const { data: balance } = await api.query.system.account(user)
+    if (balance){
+      const date = new Date()
+      let record = await AccountEntity.get(user);
+      if( record === undefined ){
+        record = new AccountEntity(user)
+        record.createdAt = date
       }
-    });
+      const balanceFrozenFee = balance.feeFrozen.toBigInt()
+      const balanceFrozenMisc = balance.miscFrozen.toBigInt()
+      const balanceReserved = balance.reserved.toBigInt()
+      const balanceFree = balance.free.toBigInt()
+      const frozen = balanceFrozenFee > balanceFrozenMisc ? balanceFrozenMisc : balanceFrozenMisc
+      const total = balanceFree + balanceReserved
+      const transferable = balanceFree - frozen
+      record.capsAmount = transferable.toString();
+      record.capsAmountFrozen = frozen.toString();
+      record.capsAmountTotal = total.toString();
+      record.capsAmountRounded = roundPrice(record.capsAmount)
+      record.capsAmountFrozenRounded = roundPrice(record.capsAmountFrozen)
+      record.capsAmountTotalRounded = roundPrice(record.capsAmountTotal)
+      record.updatedAt = date
+      await record.save();
+    }else{
+      logger.error("Error in update accout : Balance not found")
+    }
   } catch (err) {
-    logger.error(JSON.stringify(err))
+    logger.error("Error in update accout : " + err.toString())
+    if (err.sql) logger.error("Error in update accout : " + JSON.stringify(err.sql))
   }
-
 }
