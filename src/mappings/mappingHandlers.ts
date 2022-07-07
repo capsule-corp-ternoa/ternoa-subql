c2ce3a40f6036999d24216d4922d8e2a5d51d092
Victor SALOMON <victor-salomon@hotmail.fr>
2022-07-07 11:39:22 +0200

    add log to know when extrinsic is executed

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -4,6 +4,7 @@ import { getSigner, updateAccount } from "../helpers"
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
   const key = `${event.event.section}.${event.event.method}`
+  logger.info(key)
   try {
     switch (key) {
       case "balances.Transfer":
