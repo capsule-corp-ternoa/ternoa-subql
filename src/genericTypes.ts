a86323acc4a5f98f1eb934aa185f0d0288f55cad
Victor-Salomon <victor-salomon@hotmail.fr>
2024-02-15 21:34:31 +0100

    fix accounts balances

--- src/genericTypes.ts
+++ src/genericTypes.ts
@@ -1,4 +1,14 @@
+import type { Balance } from "@polkadot/types/interfaces"
+
 export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
   {
     [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>
   }[Keys]
+
+export type AccountData = {
+  free: Balance;
+  reserved: Balance;
+  frozen: Balance;
+  miscFrozen: Balance;
+  feeFrozen: Balance;
+}
